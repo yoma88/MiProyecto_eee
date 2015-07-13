8719ac5d241c1408b92c00637eff831d53330e13
yoma88 <yoma88@gmail.com>
2015-07-13 02:21:04 -0500

    Tarea

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* Autor */
+router.get('/author', function(req, res) {
+  res.render('author', { author: 'Yohana Espinoza Sólorzano' });
+});
+
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
 
